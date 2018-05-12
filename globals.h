2fbca080876493bcafc3819503b3d7121013d85e
kbuffington <kbuffington@gmail.com>
2018-05-11 22:55:42 -0500

    v3.0.0 released

--- globals.h
+++ globals.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define COMPONENT_NAME			"foo_enhanced_playcount"
-#define COMPONENT_VERSION		"2.9.0 beta 6"
+#define COMPONENT_VERSION		"3.0.0"
 
 static const GUID guid_foo_enhanced_playcount_index = { 0xc1bd000, 0x43e7, 0x4078,{ 0xb8, 0x85, 0x48, 0xee, 0x42, 0x49, 0xde, 0xc3 } };
 static const GUID guid_foo_enhanced_playcount_obsolete = { 0xc1bd000, 0x43e7, 0x4078,{ 0xb8, 0x85, 0x48, 0xee, 0x42, 0x49, 0xde, 0xc2 } };
