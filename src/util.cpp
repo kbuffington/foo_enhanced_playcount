f9521a132b4aba2192b7502d6b6356c508125e7f
kbuffington@gmail.com <kbuffington@gmail.com>
2019-11-07 22:06:18 -0600

    Timestamps returned were UTC + currentTimeZone offset which meant they were both wrong (should have returned UTC) and inaccurate when daylight savings time changed

--- src/util.cpp
+++ src/util.cpp
@@ -5,6 +5,11 @@
 namespace util {
 
 	t_uint64 timestampWindowsToJS(t_filetimestamp t) {
-		return pfc::fileTimeWtoU(t) * 1000;
+		FILETIME ft;
+		if (FileTimeToLocalFileTime((FILETIME*)& t, &ft)) {
+			t_filetimestamp ts = static_cast<__int64>(ft.dwHighDateTime) << 32 | ft.dwLowDateTime;
+			return pfc::fileTimeWtoU(ts) * 1000;
+		}
+		return 0;
 	}
 }
