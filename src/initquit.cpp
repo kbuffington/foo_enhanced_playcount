5996a5799e16f0efc86e90756425ae37c3a64d53
kbuffington@gmail.com <kbuffington@gmail.com>
2023-02-15 14:39:28 -0600

    Wait for on_library_initialized before pulling recent scrobbles

--- src/initquit.cpp
+++ src/initquit.cpp
@@ -1,13 +1,22 @@
 #include "stdafx.h"
 #include "globals.h"
+#include "foobar2000/SDK/library_callbacks.h"
 #include "PlayedTimes.h"
+
+
+class myLibraryCallbacks : public library_callback_v2_dynamic_impl_base {
+public:
+	void on_library_initialized() {
+		foo_enhanced_playcount::updateRecentScrobblesThreaded(true);
+	}
+};
 
 class myinitquit : public initquit {
 public:
 	void on_init() {
 		console::print(COMPONENT_NAME": loaded");
-		foo_enhanced_playcount::convertHashes();
-		foo_enhanced_playcount::updateRecentScrobblesThreaded(true);
+		// foo_enhanced_playcount::convertHashes();
+		new myLibraryCallbacks();
 	}
 	void on_quit() {
 		console::print(COMPONENT_NAME": unloading");
